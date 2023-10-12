3ceba9c469ba0e146480e876aca061cfa7b57703
Inas290 <145805824+Inas290@users.noreply.github.com>
2023-10-12 20:53:05 +0400

    trial-deploy.py

--- trial-deploy.py
+++ trial-deploy.py
@@ -1,82 +1,69 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 import plotly.express as px
 
-# Define data URLs
-RETAIL_DATA_URL = 'https://raw.githubusercontent.com/Inas290/hello-streamlit/main/retail_sales_dataset.csv'
-FRAUD_DATA_URL = 'https://raw.githubusercontent.com/Inas290/hello-streamlit/main/fraud1.csv'
+# Load the data from a CSV file
+df = pd.read_csv("https://raw.githubusercontent.com/Inas290/hello-streamlit/main/dataset-v2.csv")
 
-# Load your retail sales dataset
-@st.cache
-def load_retail_data():
-    data = pd.read_csv(RETAIL_DATA_URL)
-    return data
+# Create a Streamlit app
+st.set_page_config(
+    page_title="Your App Title",
+    page_icon=":chart_with_upwards_trend:",
+    layout="wide",
+)
 
-retail_data = load_retail_data()
+st.sidebar.title("Navigation")
+user_menu = st.sidebar.radio(
+    'Select an Option',
+    ('Home Page', 'Select Country', 'Create Box Plot')
+)
 
-# Create the notched horizontal box plot
-st.title('Retail Sales Analysis')
-st.subheader("Notched Horizontal Box Plot: Total Amount vs. Product Category (Colored by Gender)")
-fig = px.box(retail_data, x="Total Amount", y="Product Category", orientation="h", color="Gender", notched=True,
-             category_orders={"Product Category": ["Category1", "Category2", "Category3", "Category4"]},
-             title="Notched Horizontal Box Plot: Total Amount vs. Product Category (Colored by Gender)")
+if user_menu == 'Home Page':
+    # Display title, description, and photo for the Home Page
+    st.title("Title")
+    st.write("Tourism is a dynamic and global industry that revolves around the concept of people traveling to various destinations for leisure, adventure, or exploration. It plays a pivotal role in promoting cultural exchange, economic growth, and mutual understanding between nations. Tourists seek diverse experiences, from relaxing on exotic beaches and exploring historical landmarks to immersing themselves in the rich tapestry of different cultures. As a powerful economic driver, tourism generates revenue, creates job opportunities, and sustains local communities. It also encourages environmental conservation and the preservation of heritage sites. In essence, tourism is a vibrant and multifaceted sector that bridges cultures, fosters economic development, and kindles a sense of wonder in travelers worldwide.")
+    st.image("https://raw.githubusercontent.com/Inas290/hello-streamlit/main/tour.jpg")
 
-st.plotly_chart(fig)
+elif user_menu == "Select Country":
+    st.title("Country Data Visualization")
+    # Select the countries you want to plot (in this example, all countries)
+    countries = df['Country'].unique()
+    selected_country = st.selectbox("Select a Country", countries)
 
-# Add marginal plots (violin plot and box plot)
-st.subheader("Marginal Plots")
-fig, axs = plt.subplots(1, 2, figsize=(12, 6))
-sns.violinplot(data=retail_data, x="Gender", y="Total Amount", ax=axs[0])
-axs[0].set_title("Violin Plot")
-sns.boxplot(data=retail_data, x="Gender", y="Age", ax=axs[1])
-axs[1].set_title("Box Plot")
-st.pyplot(fig)
+    # Filter the data for the selected country
+    data_country = df[df['Country'] == selected_country]
 
-# Load your fraud dataset
-@st.cache
-def load_fraud_data():
-    data = pd.read_csv(FRAUD_DATA_URL)
-    data['trans_date_trans_time'] = pd.to_datetime(data['trans_date_trans_time'])  # Convert to datetime
-    data['trans_date_trans_time_str'] = data['trans_date_trans_time'].dt.strftime('%Y-%m-%d %H:%M:%S')
-    return data
+    # Check if data exists for the selected country
+    if data_country.empty:
+        st.write(f"No data available for {selected_country}")
+    else:
+        # Extract the variable and yearly data
+        variable = data_country['Variable'].values[0]
+        yearly_data = data_country.iloc[:, 2:]
 
-fraud_data = load_fraud_data()
+        # Convert string data to numbers (may contain commas)
+        yearly_data = yearly_data.apply(lambda x: x.str.replace(',', '').astype(float))
 
-# Error handling for the choropleth map
-try:
-    st.title('Fraud Transaction Choropleth Map')
-    st.subheader("Choropleth Map: Transaction Amount by State")
+        # Transpose the data so that years are on the x-axis
+        yearly_data = yearly_data.T
 
-    fig = px.choropleth(fraud_data,
-                        locations="state",
-                        color="amt",
-                        hover_name="merchant",
-                        animation_frame="trans_date_trans_time_str",  # Use the string column for animation frames
-                        color_continuous_scale=px.colors.sequential.Plasma,
-                        locationmode="USA-states",
-                        title="Choropleth Map: Transaction Amount by State")
+        # Create a line plot using Plotly Express
+        fig = px.line(yearly_data, x=yearly_data.index, y=yearly_data.columns, title=f'{variable} by Year in {selected_country}')
+        st.plotly_chart(fig)
 
-    # Add background USA map
-    fig.update_geos(
-        visible=False,
-        scope="usa",
-        showcoastlines=True,
-    )
+elif user_menu == "Create Box Plot":
+    st.title("Category Box Plots")
+    # Select the variable to study
+    variable = st.selectbox("Select a Variable", df['Variable'].unique())
 
-    # Modify the background map style
-    fig.update_geos(
-        lataxis_range=[24, 50],  # Adjust latitude axis range for a better view
-        lonaxis_range=[-125, -66],  # Adjust longitude axis range for a better view
-        showland=True,
-        landcolor="rgb(243, 243, 243)",  # Background color for land
-        showlakes=True,
-        lakecolor="rgb(255, 255, 255)",  # Background color for lakes
-        showocean=True,
-        oceancolor="rgb(230, 255, 255)",  # Background color for oceans
-    )
+    # Filter the data for the selected variable
+    data_variable = df[df['Variable'] == variable]
 
-    st.plotly_chart(fig)
-except Exception as e:
-    st.error(f"An error occurred: {e}")
+    # Check if data exists for the selected variable
+    if data_variable.empty:
+        st.write(f"No data available for {variable}")
+    else:
+        # Create a notched horizontal box plot using Plotly Express
+        fig = px.box(data_variable, x='Value', y='Category', color='Category', title=f'Box Plot of {variable} by Category')
+        st.plotly_chart(fig)
